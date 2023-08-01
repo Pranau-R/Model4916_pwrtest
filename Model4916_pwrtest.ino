f9a9c00fbe8edbd006c1d110354525212d2a3f83
Pranau-R <pranau@mcci.com>
2023-08-01 14:59:27 +0530

    optimized the sketch by removing unwanted modules

--- Model4916_pwrtest.ino
+++ Model4916_pwrtest.ino
@@ -27,13 +27,6 @@ Author:
 #include <Catena_Log.h>
 #include <Catena_Mx25v8035f.h>
 #include <mcciadk_baselib.h>
-#include <MCCI_Catena_SCD30.h>
-#include <bsec.h>
-#include <Catena-SHT3x.h>
-#include <MCCI_Catena_SCD30.h>
-#include <MCCI_Catena_IPS-7100.h>
-#include <MCCI_Catena_ADS131M04.h>
-#include <MCCI_Catena_SAM-M8Q.h>
 #include <Catena_PollableInterface.h>
 
 #include <SPI.h>
@@ -43,11 +36,6 @@ Author:
 
 using namespace McciCatena;
 using namespace McciModel4916;
-using namespace McciCatenaSht3x;
-using namespace McciCatenaAds131m04;
-using namespace McciCatenaIps7100;
-using namespace McciCatenaSamM8q;
-using namespace McciCatenaScd30;
 
 /****************************************************************************\
 |
@@ -55,28 +43,28 @@ using namespace McciCatenaScd30;
 |
 \****************************************************************************/
 
-constexpr uint8_t kFramPowerOn = D10;
+constexpr uint8_t kBoosterEnable = D10;
 constexpr uint8_t kBoosterPowerOn = D12;
 
-static inline void framPowerOn(void)
+static inline void BoosterEnable(void)
     {
-    pinMode(kFramPowerOn, OUTPUT);
-    digitalWrite(kFramPowerOn, HIGH);
+    pinMode(kBoosterEnable, OUTPUT);
+    digitalWrite(kBoosterEnable, HIGH);
     }
 
-static inline void framPowerOff(void)
+static inline void BoosterDisable(void)
     {
-    pinMode(kFramPowerOn, INPUT);
-    digitalWrite(kFramPowerOn, LOW);
+    pinMode(kBoosterEnable, INPUT);
+    digitalWrite(kBoosterEnable, LOW);
     }
 
-static inline void BoosterPowerOn(void)
+static inline void BoosterPowerOff(void)
     {
     pinMode(kBoosterPowerOn, OUTPUT);
     digitalWrite(kBoosterPowerOn, HIGH);
     }
 
-static inline void BoosterPowerOff(void)
+static inline void BoosterPowerOn(void)
     {
     pinMode(kBoosterPowerOn, INPUT);
     digitalWrite(kBoosterPowerOn, LOW);
@@ -106,24 +94,6 @@ c4916Gpios gpiopower    { &i2cgpiopower };
 // the global I2C GPIO object for enabling communication
 cPCA9574                i2cgpioenable    { &Wire, 1 };
 c4916Gpios gpioenable   { &i2cgpioenable };
-
-// BME680 Environmental sensor
-Bsec                    gBme;
-
-// The temperature/humidity sensor
-cSHT3x gSht3x           {Wire};
-bool fSht3x;
-
-cSCD30 gScd             { Wire };
-
-// the Particle sensor
-cIPS7100 gIps           {Wire};
-
-// the ADC for spec sensor
-cADS131M04 gAds;
-
-// GPS sensor
-cSAM_M8Q gGps;
 
 McciCatena::cFram8k gFram;
 
@@ -200,17 +170,13 @@ Returns:
 
 void setup(void)
     {
-    framPowerOn();
-    BoosterPowerOn();
     Serial.begin(115200);
     Wire.begin();
-    setup_platform();
     setup_gpio();
+    setup_platform();
 
     // set up flash
     setup_flash();
-    // setup sensors
-    setup_sensors();
     setup_commands();
     }
 
@@ -239,21 +205,14 @@ void setup_platform()
     gCatena.begin();
     gLoRaWAN.begin(&gCatena);
 
-    delay(5000);
-
     // power-up Flash
     gpiopower.configVSpi(true);
     delay(2);
     gpiopower.setVSpi(true);
 
-    // if running unattended, don't wait for USB connect.
-    if (!(gCatena.GetOperatingFlags() &
-        static_cast<uint32_t>(gCatena.OPERATING_FLAGS::fUnattended)))
-        {
-        while (!Serial)
-            /* wait for USB attach */
-            yield();
-        }
+    while (!Serial)
+        /* wait for Serial to begin */
+        yield();
 
     gCatena.SafePrintf("\n");
     gCatena.SafePrintf("-------------------------------------------------------------------------------\n");
@@ -261,7 +220,7 @@ void setup_platform()
     gCatena.SafePrintf("The program will now idle waiting for you to enter commands\n");
     gCatena.SafePrintf("Enter 'help' for a list of commands.\n");
     gCatena.SafePrintf("(remember to select 'Line Ending: Newline' at the bottom of the monitor window.)\n");
-    
+
 #ifdef CATENA_CFG_SYSCLK
     gCatena.SafePrintf("SYSCLK: %d MHz\n", CATENA_CFG_SYSCLK);
 #endif
@@ -305,11 +264,12 @@ void setup_commands()
 
 void setup_flash(void)
     {
+    // gSPI2.begin();
     if (gFlash.begin(&gSPI2, Catena::PIN_SPI2_FLASH_SS))
         {
         gfFlash = true;
         gFlash.powerDown();
-        // gCatena.SafePrintf("FLASH found, put power down\n");
+        gCatena.SafePrintf("FLASH found, put power down\n");
         }
     else
         {
@@ -318,203 +278,8 @@ void setup_flash(void)
         gSPI2.end();
         gCatena.SafePrintf("No FLASH found: check hardware\n");
         }
-    }
 
-void setup_sensors()
-    {
-    setup_bme680();
-    setup_sht3x();
-    setup_scd30();
-    setup_gps();
-    setup_piera();
-    setup_adc();
-    }
-
-void setup_sht3x(void)
-    {
-    if (gSht3x.begin())
-        {
-        fSht3x = true;
-        gCatena.SafePrintf("SHT3x found: Env sensor\n");
-        }
-    else
-        {
-        fSht3x = false;
-        gCatena.SafePrintf("No SHT-3x found: check hardware\n");
-        }
-    }
-
-void setup_gps()
-    {
-    // powerup GPS
-    gpiopower.configVgps(true);
-    gpioenable.configGps(true);
-    gpiopower.setVgps(true);
-    delay(5);
-    gpioenable.enableGps(true);
-    delay(5);
-
-    if (gGps.begin())
-        {
-        gCatena.SafePrintf("SAM-M8Q found: GPS location and time\n");
-        configGps();
-        gGps.powerSaveMode(true);
-        }
-    else
-        {
-        gCatena.SafePrintf("No SAM-M8Q GPS found: check wiring\n");
-        }
-    }
-
-void configGps()
-    {
-    uint8_t ret;
-
-    if (gGps.setmaxStartupStateDur(25))
-      gCatena.SafePrintf("configGps: Set Max StartUp Duration failed\n");
-
-    if (gGps.setminAcqTime(5000))
-      gCatena.SafePrintf("configGps: Set Min Acquisition Time failed\n");
-
-    if (gGps.setUpdatePeriod(5000))
-      gCatena.SafePrintf("configGps: Set Min Acquisition Time failed\n");
-
-    if (gGps.setSearchPeriod(15000))
-      gCatena.SafePrintf("configGps: Set Min Acquisition Time failed\n");
-
-    gCatena.SafePrintf("configGps: GPS Max Startup time : %u seconds\n", gGps.getmaxStartupStateDur());
-    gCatena.SafePrintf("configGps: GPS Min Acq time : %u seconds\n", gGps.getminAcqTime()/1000);
-    gCatena.SafePrintf("configGps: GPS Update Period: %u ms\n", gGps.getUpdatePeriod());
-    gCatena.SafePrintf("configGps: GPS Search Period: %u ms\n", gGps.getSearchPeriod());
-
-    // Set the I2C port to output
-    gGps.setI2COutput(COM_TYPE_UBX);
-    delay (100);
-
-    // Save Configuration
-    gGps.saveConfiguration();
-    }
-
-void setup_scd30()
-    {
-    // power-up and enable FRAM
-    gpiopower.configVqwiic(true);
-    gpioenable.configQwiic(true);
-    gpiopower.setVqwiic(true);
-    delay(20);
-    gpioenable.enableQwiic(true);
-    delay(20);
-
-    if (! gScd.begin())
-        {
-        gCatena.SafePrintf("No SCD30 found! Begin failed: %s(%u)\n",
-            gScd.getLastErrorName(),
-            unsigned(gScd.getLastError())
-            );
-        }
-    else
-        {
-        gCatena.SafePrintf("SCD30 found! CO2 sensor\n");
-        printSCDinfo();
-        delay(5000);
-        }
-
-    gpioenable.enableQwiic(false);
-    gpiopower.setVqwiic(false);
-    delay(2);
-    }
-
-void printSCDinfo()
-    {
-    auto const info = gScd.getInfo();
-    gCatena.SafePrintf(
-        "Found sensor: firmware version %u.%u\n",
-        info.FirmwareVersion / 256u,
-        info.FirmwareVersion & 0xFFu
-        );
-    gCatena.SafePrintf("  Automatic Sensor Calibration: %u\n", info.fASC_status);
-    gCatena.SafePrintf("  Sample interval:      %6u secs\n", info.MeasurementInterval);
-    gCatena.SafePrintf("  Forced Recalibration: %6u ppm\n", info.ForcedRecalibrationValue);
-    gCatena.SafePrintf("  Temperature Offset:   %6d centi-C\n", info.TemperatureOffset);
-    gCatena.SafePrintf("  Altitude:             %6d meters\n", info.AltitudeCompensation);
-    }
-
-void setup_piera()
-    {
-    // power-up and enable FRAM
-    BoosterPowerOn();
-    delay(100);
-
-    gpiopower.configVPiera(true);
-    gpioenable.configPiera(true);
-    gpiopower.setVPiera(true);
-    delay(2);
-    gpioenable.enablePiera(true);
-    delay(2);
-
-    if (gIps.begin())
-        {
-        gCatena.SafePrintf("IPS-7100 found: Particle sensor\n");
-        }
-    else
-        {
-        gCatena.SafePrintf("No IPS-7100 found: check wiring\n");
-        }
-
-    gpioenable.enablePiera(false);
-    gpiopower.setVPiera(false);
-    delay(2);
-    BoosterPowerOff();
-    }
-
-void setup_adc()
-    {
-    // power-up ADS131M04
-    gpiopower.configVSpi(true);
-    gpiopower.setVSpi(true);
-    delay(20);
-
-    if (gAds.begin(&gSPI2, D16, D17, D18))
-        {
-        gAds.globalChop(true);
-        gCatena.SafePrintf("ADS131M04 found: Analog-to-digital (SPEC sensors)\n");
-        }
-    else
-        {
-        gCatena.SafePrintf("No ADS131M04 found: check wiring\n");
-        }
-
-    gpiopower.configVSpi(false);
     gpiopower.setVSpi(false);
-    delay(20);
-    }
-
-void setup_bme680()
-    {
-    // power-up and enable FRAM
-    gpiopower.configVtvoc(true);
-    gpioenable.configTvoc(true);
-    gpiopower.setVtvoc(true);
-    delay(20);
-    gpioenable.enableTvoc(true);
-    delay(2);
-
-    gBme.begin(BME680_I2C_ADDR_SECONDARY, Wire);
-    printBME680info();
-
-    gpioenable.enableTvoc(false);
-    gpiopower.setVtvoc(false);
-    delay(2);
-    }
-
-void printBME680info()
-    {
-    gCatena.SafePrintf("\nBSEC library version %d.%d.%d.%d\n",
-        gBme.version.major,
-        gBme.version.minor,
-        gBme.version.major_bugfix,
-        gBme.version.minor_bugfix
-        );
     }
 
 /*
@@ -694,102 +459,102 @@ cCommandStream::CommandStatus cmdSleep(
     int argc,
     char **argv
     )
-        {
-        if (argc > 2)
-                return cCommandStream::CommandStatus::kInvalidParameter;
+    {
+    if (argc > 2)
+        return cCommandStream::CommandStatus::kInvalidParameter;
 
-        // get arg 1 as sleep interval, default is 5 seconds.
-        cCommandStream::CommandStatus status;
-        uint32_t sleepInterval;
-        status = cCommandStream::getuint32(argc, argv, 1, /* base */ 0, sleepInterval, 5);
+    // get arg 1 as sleep interval, default is 5 seconds.
+    cCommandStream::CommandStatus status;
+    uint32_t sleepInterval;
+    status = cCommandStream::getuint32(argc, argv, 1, /* base */ 0, sleepInterval, 5);
 
-        if (status != cCommandStream::CommandStatus::kSuccess)
-                return status;
-
-        pThis->printf("%s for %u seconds\n", argv[0], sleepInterval);
-        delay(2000);
-
-        gpioenable.enableVdd1(false);
-        gpiopower.setVdd1(false);
-
-        LedPattern const save_led = gLed.Set(LedPattern::Off);
-        Serial.end();
-        Wire.end();
-        SPI.end();
-        if (gfFlash)
-        	gSPI2.end();
-
-        framPowerOff();
-        gCatena.Sleep(sleepInterval);
-
-        framPowerOn();
-        Serial.begin();
-        Wire.begin();
-        SPI.begin();
-        // if (gfFlash)
-        // 	gSPI2.begin();
-
-        // power-up FRAM
-        gpiopower.configVdd1(true);
-        gpioenable.configVdd(true);
-        gpiopower.setVdd1(true);
-        delay(2);
-        gpioenable.enableVdd1(true);
-        delay(2);
-
-        gLed.Set(save_led);
-        pThis->printf("awake again\n");
-
-        // start the FRAM
-        if (! gFram.begin())
-            {
-            gLog.printf(
-                gLog.kError,
-                "CatenaStm32L0::begin: FRAM begin() failed\n"
-                );
-            }
-
-        // check whether the FRAM is valid
-        if (! gFram.isValid())
-            {
-            gLog.printf(
-                gLog.kError,
-                "CatenaStm32L0::begin: FRAM contents are not valid, resetting\n"
-                );
-            gFram.initialize();
-            }
-
+    if (status != cCommandStream::CommandStatus::kSuccess)
         return status;
+
+    pThis->printf("%s for %u seconds\n", argv[0], sleepInterval);
+    delay(2000);
+
+    BoosterPowerOff();
+    gpioenable.enableVdd1(false);
+    gpiopower.setVdd1(false);
+
+    LedPattern const save_led = gLed.Set(LedPattern::Off);
+    Serial.end();
+    Wire.end();
+    SPI.end();
+    if (gfFlash)
+        gSPI2.end();
+
+    gCatena.Sleep(sleepInterval);
+
+    Serial.begin();
+    Wire.begin();
+    SPI.begin();
+    // if (gfFlash)
+    // 	gSPI2.begin();
+
+    // power-up FRAM
+    gpiopower.configVdd1(true);
+    gpioenable.configVdd(true);
+    gpiopower.setVdd1(true);
+    delay(2);
+    gpioenable.enableVdd1(true);
+    delay(2);
+    BoosterPowerOn();
+
+    gLed.Set(save_led);
+    pThis->printf("awake again\n");
+
+    // start the FRAM
+    if (! gFram.begin())
+        {
+        gLog.printf(
+            gLog.kError,
+            "CatenaStm32L0::begin: FRAM begin() failed\n"
+            );
         }
+
+    // check whether the FRAM is valid
+    if (! gFram.isValid())
+        {
+        gLog.printf(
+            gLog.kError,
+            "CatenaStm32L0::begin: FRAM contents are not valid, resetting\n"
+            );
+        gFram.initialize();
+        }
+
+    return status;
+    }
 
 // argv[0] is "standby"
 /* process "standby"  */
 cCommandStream::CommandStatus cmdStandby(
-        cCommandStream *pThis,
-        void *pContext,
-        int argc,
-        char **argv
-        )
-        {
-        if (argc > 1)
-                return cCommandStream::CommandStatus::kInvalidParameter;
+    cCommandStream *pThis,
+    void *pContext,
+    int argc,
+    char **argv
+    )
+    {
+    if (argc > 1)
+            return cCommandStream::CommandStatus::kInvalidParameter;
 
-        pThis->printf("%s not implemented yet\n", argv[0]);
-        return cCommandStream::CommandStatus::kSuccess;
-        }
+    pThis->printf("%s not implemented yet\n", argv[0]);
+    return cCommandStream::CommandStatus::kSuccess;
+    }
 
 // argv[0] is "stop"
 /* process "stop"  */
 cCommandStream::CommandStatus cmdStop(
-        cCommandStream *pThis,
-        void *pContext,
-        int argc,
-        char **argv
-        )
-        {
-        if (argc > 1)
-                return cCommandStream::CommandStatus::kInvalidParameter;
+    cCommandStream *pThis,
+    void *pContext,
+    int argc,
+    char **argv
+    )
+    {
+    if (argc > 1)
+            return cCommandStream::CommandStatus::kInvalidParameter;
 
-        pThis->printf("%s not implemented yet\n", argv[0]);
-        return cCommandStream::CommandStatus::kSuccess;
-        }
+    pThis->printf("%s not implemented yet\n", argv[0]);
+    return cCommandStream::CommandStatus::kSuccess;
+    }
